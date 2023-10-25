2acd3a46a4b0268f0f5dc7cb18b4c89a198069c8
Luiz Felipe Radoll Cordeiro <79219125+XT07@users.noreply.github.com>
2023-10-24 21:30:02 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const btnMobile =document.getElementById("btn-mobile");
 
 function toggleMenu() {
-    
+    const ulResp =document.getElementById("ulResp");
+    ulResp.classList.toggle("active");
 }
 
 btnMobile.addEventListener("click", toggleMenu);
